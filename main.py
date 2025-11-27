4cd81bc0d233792a0f1c9d5a0e79deec9f1bdf1f
eden <eden.leblanc@edu.esiee.fr>
2025-11-27 11:14:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-#### Imports et définition des variables globales
-
+"""
+Module de lecture et de manipulation de données.
+Contient la fonction read_data() pour lire un fichier CSV
+et des fonctions pour manipuler des listes numériques.
+"""
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
 
-def read_data(filename: str) -> list[list[int]]:
+def read_data(filename: str):
     """retourne le contenu du fichier <filename>
 
     Args:
@@ -15,11 +18,15 @@ def read_data(filename: str) -> list[list[int]]:
     """
     l = []
     with open(filename, mode='r', encoding='utf8') as f:
-        for line in f.readlines():
-            clean = line.strip()
-            clean = clean.split(';')
-            liste = [int(s) for s in clean]
-            l.append([liste])
+        lines = f.readlines()
+        for line in lines:
+            clean = line.strip().split(';')
+            liste = []
+            for s in clean:
+                s = s.strip()
+                if s:
+                    liste.append(int(s))
+            l.append(liste)
     return l
 
 def get_list_k(data: list[list[int]], k: int) -> list[int]:
@@ -28,19 +35,28 @@ def get_list_k(data: list[list[int]], k: int) -> list[int]:
 
 def get_first(l):
     ''' return first element of list l'''
-    return l[0]
+    if l:
+        return l[0]
+    return None
 
 def get_last(l):
     ''' return last element of list l'''
-    return l[len(l)]
+    if l:
+        return l[-1]
+    return None
 
 def get_max(l):
     ''' return max element of list l'''
-    return max(l)
+    if l:
+        return max(l)
+    return None
+
 
 def get_min(l):
     ''' return min element of list l'''
-    return min(l)
+    if l:
+        return min(l)
+    return None
 
 def get_sum(l):
     ''' return sum of list l'''
@@ -51,6 +67,7 @@ def get_sum(l):
 
 
 def main():
+    '''function main'''
     data = read_data(FILENAME)
     for i, l in enumerate(data):
         print(i, l)
